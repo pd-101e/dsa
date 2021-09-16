692f0c220be1d611cfa1637d59034dfdff4fb9d8
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2021-09-16 22:49:50 +0530

    Create q4.cpp

---
+++ arrays/q4.cpp
@@ -0,0 +1,35 @@
+class Solution{
+    public:
+    //dutch national flag
+    
+    void sort012(int arr[], int n)
+    {
+        // coode here 
+        
+        int low = 0 ;
+        int mid = 0 ;
+        int high = n-1;
+        
+        while(mid<=high)
+        {
+            if(arr[mid]==0)
+            {
+                swap(arr[mid],arr[low]);
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1)
+            {
+                mid++;
+            }
+            else 
+            {
+                swap(arr[mid],arr[high]);
+                high--;
+            }
+            
+            
+        }
+        
+    }
+};
