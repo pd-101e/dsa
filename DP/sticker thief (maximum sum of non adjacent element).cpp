112a5b3b358d9a6b1f0b9a6cf7193596e4960c36
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2021-09-29 17:40:34 +0530

    Update sticker thief (maximum sum of non adjacent element).cpp

--- DP/sticker thief (maximum sum of non adjacent element).cpp
+++ DP/sticker thief (maximum sum of non adjacent element).cpp
@@ -1,3 +1,6 @@
+/// simple one 
+
+
 {
     public:
     //Function to find the maximum money the thief can get.
@@ -21,3 +24,69 @@
         
     }
 };
+
+
+
+//dp method
+class solution{
+
+    public:
+    //Function to find the maximum money the thief can get.
+    int solve(int arr[], int n )
+{
+    int dp[n+1];
+    if(n>=0)
+    dp[0] = 0;
+    if(n>=1)
+    {
+        dp[1] = arr[0];
+    }
+    if(n>=2)
+    {
+        dp[2] = max(arr[0],arr[1]);
+    }
+    for(int i = 3 ; i<=n ; i++)
+    {
+        dp[i] = max(dp[i-2]+arr[i-1],dp[i-1]);
+    }
+    return dp[n];
+    
+   
+}
+    
+    int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        return solve(arr,n);
+        
+    }
+
+
+};
+
+
+/// recursive method
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int arr[], int n )
+{
+    if(n<=0)
+    {
+        return 0;
+    }
+    
+    return max(arr[n-1]+solve(arr,n-2),solve(arr,n-1));
+}
+
+
+
+
+
+
+int main()
+{
+    int arr[] = {1,2,3};
+    int n = 3 ;
+    cout<<endl<<solve(arr,n)<<endl;
+}
