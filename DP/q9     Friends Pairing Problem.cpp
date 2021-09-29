c9d48d0787376769df70718b72369ab5198f1104
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2021-09-29 17:59:27 +0530

    Update and rename q9.cpp to q9     Friends Pairing Problem.cpp

--- DP/q9     Friends Pairing Problem.cpp
+++ DP/q9     Friends Pairing Problem.cpp
@@ -17,3 +17,10 @@ public:
         
     }
 }; 
+
+// Reccursion
+int countFriendsPairings(int n) 
+{ 
+    if (n<=1) return 1;
+    return countFriendsPairings(n-1) + (n-1) * countFriendsPairings(n-2);   // issue of overflow will be there
+}
