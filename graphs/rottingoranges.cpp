fb95e885e5f953a20b07bd7b6488941085a96587
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2021-09-30 17:32:14 +0530

    Update rottingoranges.cpp

--- graphs/rottingoranges.cpp
+++ graphs/rottingoranges.cpp
@@ -6,53 +6,117 @@ public:
         return (r>=0 and c>=0 and r<m and c<n );
     }
     
-    
-    
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+    int orangesRotting(vector<vector<int>>& grid) {
         
-        vector<int>dx = {-1,0,1,0};
-        vector<int>dy = {0,1,0,-1};
+        vector<int> dx = {0,0,-1,1};
+        vector<int> dy = {1,-1,0,0};
         
-        int m = mat.size();
-        int n = mat[0].size();
-        if(m==0) return {};
-        queue<pair<int,int>>q;
+        int m = grid.size();
+        int n = grid[0].size();
+        if(m==0)
+        {
+            return {};
+        }
+        
+        
+        
         vector<vector<int>>dist(m,vector<int>(n,-1));
+        queue<pair<int,int>>q;
+        
+        bool flag = false;
         for(int i = 0 ; i < m ; i++)
         {
             for(int j = 0 ; j<n ; j++)
             {
-                if(mat[i][j] == 0)
+                if(grid[i][j] == 2)
                 {
                     q.push({i,j});
+                    dist[i][j] = 2;
+                     flag = true;
+                }
+                if(grid[i][j]==0)
+                {
                     dist[i][j] = 0;
                 }
                 
             }
         }
+       if(flag == false)
+       {
+           if(m==1)
+        {
+            for(int i = 0 ; i<n ; i++)
+            {
+                if(grid[0][i]==1)
+                {
+                    return -1;
+                }
+            }
+            return 0;
+        }
+        
+         if(n==1)
+        {
+            for(int i = 0 ; i<m ; i++)
+            {
+                if(grid[i][0]==1)
+                {
+                    return -1;
+                }
+            }
+            return 0;
+        }
+       }
         
         while(!q.empty())
         {
+            
             auto p = q.front();
             int r = p.first;
             int c = p.second;
             q.pop();
             for(int i = 0 ; i<4 ; i++)
             {
-                int nr = r+ dx[i];
+                int nr = r + dx[i];
                 int nc = c + dy[i];
+                
                 if(isvalid(nr,nc,m,n))
                 {
-                    if(mat[nr][nc]==1 and dist[nr][nc]==-1)
+                    if(grid[nr][nc] == 1 and dist[nr][nc] == -1 )
                     {
                         dist[nr][nc] = dist[r][c] + 1;
                         q.push({nr,nc});
                     }
                 }
+                
             }
             
+            
+            
+        }
+        // for(int i = 0 ; i<m ; i++)
+        // {
+        //     for(int j = 0 ; j<n ; j++)
+        //     {
+        //         cout<<dist[i][j]<<"  ";
+        //     }
+        //     cout<<endl;
+        // }
+        
+        int ans = 0;
+        for(int i = 0 ; i<m ; i++)
+        {
+            for(int j = 0 ; j<n ; j++)
+            {
+                if(dist[i][j] == -1)
+                {
+                    return -1;
+                }
+                ans = max(dist[i][j],ans);
+            }
         }
         
-        return dist;
+        //cout<<endl<<ans<<endl;
+        return ans - 2 ;
     }
 };
