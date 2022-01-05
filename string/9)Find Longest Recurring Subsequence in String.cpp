f5a3b96ea1eb8ace4a657e45425ab2b13cab170c
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2022-01-05 20:29:07 +0530

    Create 9)Find Longest Recurring Subsequence in String.cpp

---
+++ string/9)Find Longest Recurring Subsequence in String.cpp
@@ -0,0 +1,44 @@
+class Solution{
+	public:
+	
+	    int solve(string s1 , string s2)
+	    {
+	        int n = s1.length();
+	        
+	        int dp[n+1][n+1];
+	        for(int i = 0 ; i<=n ; i++)
+	        {
+	            for(int j = 0 ; j<=n ; j++)
+	            {
+	                dp[i][j] = 0;
+	            }
+	        }
+	        for(int i = 1 ; i<=n ; i++)
+	        {
+	            for(int j = 1 ; j<=n ; j++)
+	            {
+	                if( s1[i-1] == s2[j-1] and i!=j ) // kee p in mind of i-1
+	                {
+	                    dp[i][j] =  1 + dp[i-1][j-1];
+	                }
+	                else 
+	                {
+	                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+	                }
+	            }
+	            
+	        }
+	        
+	        return dp[n][n];
+	    }
+	
+	    
+		int LongestRepeatingSubsequence(string str){
+		    // Code here
+		    
+		    
+		    
+		    return solve(str,str);
+		}
+
+};
