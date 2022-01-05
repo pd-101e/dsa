8e8acdc7e15ae27c9384cda4b21de2e77dc96206
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2022-01-05 21:55:46 +0530

    Create 11) Print all the permutations of the given string.cpp

---
+++ string/11) Print all the permutations of the given string.cpp
@@ -0,0 +1,42 @@
+
+class Solution{
+
+	public:
+	
+	void solve(vector<string>&ans , string &s, bool visited[] , string &temp )
+	{
+	    if(temp.length() == s.length())
+	    {
+	        ans.push_back(temp);
+	        return;
+	    }
+	    for(int i = 0 ; i<s.length(); i++)
+	    {
+	        if(!visited[i])
+	        {
+	            visited[i] = true;
+	            temp.push_back(s[i]);
+	            solve(ans,s,visited,temp);
+	            temp.pop_back();
+	            visited[i] = false;
+	        }
+	    }
+	    return;
+	}
+	
+		vector<string>find_permutation(string S)
+		{
+		    // Code here there
+		    vector<string> ans;
+		    int n = S.length();
+		    bool visited[n];
+		    for(int i = 0 ; i<n ; i++)
+		    {
+		       visited[i] = false;
+		    }
+		    string temp ="";
+		    solve(ans,S,visited,temp);
+		    sort(ans.begin(),ans.end());
+		    return ans;
+		}
+};
