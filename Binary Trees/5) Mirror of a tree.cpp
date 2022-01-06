8192c10276c470258aa747ff9452dd713189d994
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2022-01-06 18:43:48 +0530

    Update 5) Mirror of a tree.cpp

--- Binary Trees/5) Mirror of a tree.cpp
+++ Binary Trees/5) Mirror of a tree.cpp
@@ -37,3 +37,24 @@ class Solution {
         
     }
 };
+
+// Approach 2: call the function with two root nodes and reccurse it for left of original and right of new tree and vice versa
+
+class Solution {
+public:
+    void invertHelper(TreeNode* r1, TreeNode* m) {
+        if(r1->right) {
+            m->left = new TreeNode(r1->right->val);
+            invertHelper(r1->right, m->left);
+        }
+        if(r1->left) {
+            m->right = new TreeNode(r1->left->val);
+            invertHelper(r1->left, m->right);
+        }
+    }
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return NULL;
+        TreeNode* m = new TreeNode(root->val);
+        invertHelper(root, m);
+    }
+};
