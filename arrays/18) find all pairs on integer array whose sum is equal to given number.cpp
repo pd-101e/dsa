331f749273be955dcdde0148dd594c9667d50997
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2022-01-10 21:17:48 +0530

    Update 18) find all pairs on integer array whose sum is equal to given number.cpp

--- arrays/18) find all pairs on integer array whose sum is equal to given number.cpp
+++ arrays/18) find all pairs on integer array whose sum is equal to given number.cpp
@@ -1,3 +1,4 @@
+// Approach 1:  Hash
 class Solution{   
 public:
     int getPairsCount(int arr[], int n, int k) {
@@ -13,5 +14,29 @@ public:
              mp[arr[i]]++;
         }
         return ans;
+    }
+};
+
+// Approach 2: Sort the array. Using two pointer approach count the possible pairs. (TC-O(nlogn) SC-O(1))
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        sort(arr, arr+n);
+        
+        int l=0, r=n-1, result=0;
+        while(l<r) {
+            if(arr[l]+arr[r] == k) {
+                int temp = r;
+                while(temp>l && arr[temp] == arr[r]) {
+                    temp--;result++;
+                }
+                l++;
+            } else if (arr[l]+arr[r] < k) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return result;
     }
 };
