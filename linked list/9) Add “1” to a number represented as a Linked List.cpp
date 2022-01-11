94c5398729eb491a3395ce5a35f605ea4c486358
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2022-01-11 15:34:05 +0530

    Create 9) Add “1” to a number represented as a Linked List.cpp

---
+++ linked list/9) Add “1” to a number represented as a Linked List.cpp
@@ -0,0 +1,60 @@
+
+/* 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    public:
+    
+    Node* reverse(Node* head)
+    {
+        if(head == NULL or head -> next == NULL)
+        {
+            return head;
+        }
+        Node* resthead = reverse(head -> next);
+        Node* resttail = head -> next;
+        resttail -> next = head;
+        head -> next = NULL;
+        return resthead;
+    }
+    
+    
+   Node* addOne(Node *head) 
+        { 
+            head = reverse(head);           // reversing list to make addition easy
+            
+            Node* current = head;
+            int carry = 1;
+            
+            while(carry)
+            {
+                current->data += 1;         // adding one to current node
+                
+                if(current->data < 10) return reverse(head);
+                    // if no carry we can reverse back list and return it
+                else current->data = 0;
+                    // else we continue with taking carry forward
+                
+                if(current->next == NULL) break;
+                    // if, end of list, we break from loop
+                else current = current->next;
+                    // else we move to next node
+            }
+            
+            current->next = new Node(1);  // adding new node for the carried 1
+            return reverse(head);
+        }
+};
