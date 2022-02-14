92bf0cb6fedf931d9c07ae2607ac3b39d053bc3a
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2022-02-15 01:09:03 +0530

    Update 31) Smallest Subarray with sum greater than a given value.cpp

--- arrays/31) Smallest Subarray with sum greater than a given value.cpp
+++ arrays/31) Smallest Subarray with sum greater than a given value.cpp
@@ -1,3 +1,4 @@
+// Approach 1: Niave approach- In two nested loops, check the sum of all possible subarrays. (TC-O(n^2))
 //https://leetcode.com/problems/minimum-size-subarray-sum/submissions/
 
 class Solution {
@@ -80,3 +81,27 @@ public:
     }
 };
 
+
+
+/// copied
+// Approach 1: Niave approach- In two nested loops, check the sum of all possible subarrays. (TC-O(n^2))
+
+// Approach 2: two pointer approach- refer code below. (TC-O(n))
+int sb(int arr[], int n, int x)
+{
+    int sum=0, i=0, j=-1, minsubarray=n+1;
+    while(1) {
+        if(sum > x) {
+            if(j-i+1 < minsubarray) {
+                minsubarray = j-i+1;
+            }
+            sum-=arr[i];
+            i++;
+        } else {
+            if(j==n-1) break;
+            j++;
+            sum+=arr[j];
+        }
+    }
+    return minsubarray;
+}
