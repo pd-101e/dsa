dc9caba00913f075c65c5eba8c2a6aedac4b57ae
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2022-02-14 21:51:47 +0530

    Update and rename 25) Given an array of size n and a number k, fin all elements that appear more than " n by k " times. to 25) Given an array of size n and a number k, fin all elements that appear more than " n by k " times.cpp

--- arrays/25) Given an array of size n and a number k, fin all elements that appear more than " n by k " times.cpp
+++ arrays/25) Given an array of size n and a number k, fin all elements that appear more than " n by k " times.cpp
@@ -15,3 +15,55 @@ The main step is step 2, how to maintain (k-1) potential candidates at every poi
 */
 
 // Approach 4: Hash all the elements and then iterate through the hash map and print the elements with count more than n/k. (TC-O(n))
+
+
+
+
+// C++ code to find elements whose
+// frequency yis more than n/k
+#include<bits/stdc++.h>
+using namespace std;
+
+void morethanNbyK(int arr[], int n, int k)
+{
+	int x = n / k;
+	
+	// unordered_map initialization
+	unordered_map<int, int> freq;
+
+	for(int i = 0; i < n; i++)
+	{
+		freq[arr[i]]++;
+	}
+
+	// Traversing the map
+	for(auto i : freq)
+	{
+		
+		// Checking if value of a key-value pair
+		// is greater than x (where x=n/k)
+		if (i.second > x)
+		{
+			
+			// Print the key of whose value
+			// is greater than x
+			cout << i.first << endl;
+		}
+	}
+}
+
+// Driver Code
+int main()
+{
+	int arr[] = { 1, 1, 2, 2, 3, 5,
+				4, 2, 2, 3, 1, 1, 1 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int k = 4;
+	
+	morethanNbyK(arr, n, k);
+
+	return 0;
+}
+
+// This code is contributed by chayandas018
+
