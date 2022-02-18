17486821fee42af5e7ad3a91498a58c5b80fc4cc
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2022-02-18 18:19:29 +0530

    Create 21) Find minimum swaps required to convert a Binary tree into BST.cpp

---
+++ Binary Trees/21) Find minimum swaps required to convert a Binary tree into BST.cpp
@@ -0,0 +1,47 @@
+// find the inorder traversal of the array representation of the binary tree
+// find the min swaps to convert it into sorted array (as inorder traversal of bst is increasing order)
+
+// for the last step the ques is same as Sorting and searching q18
+class Solution 
+{
+    public:
+    //Function to find the minimum number of swaps required to sort the array. 
+    // approach 
+    /*
+    vector of pair banaya aur fir usse sort karidye  pair(number , index)
+    fir uss sorted order ko original me convert kardiya 
+    */
+    
+	int minSwaps(vector<int>&nums)
+	{
+	    // Code here
+	    int n = nums.size();
+	    vector<pair<int,int>>arr(n);
+	    for(int i = 0 ; i<n ; i++)
+	    {
+	        arr[i].first = nums[i];
+	        arr[i].second = i;
+	    }
+	    sort(arr.begin() , arr.end());
+	    // lets iterate over the arr now 
+	    int minswaps = 0;
+	   // for(int i = 0 ; i<n ; i++)
+	    for(int i = 0 ; i<n ; )
+	    {
+	        if(arr[i].second == i)
+	        {
+	           // continue;
+	           i++;
+	        }
+	        else {
+	            swap(arr[i] , arr[arr[i].second]);
+	            minswaps++;
+	           // i--;
+	        }
+	    }
+	        
+	    return minswaps;
+	    
+	    
+	}
+};
