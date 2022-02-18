8446a1d6c6b2ef94c0c9e44ca03c0459e3a2d25c
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2022-02-19 02:56:42 +0530

    Create 38) Word Break Problem.cpp

---
+++ DP/38) Word Break Problem.cpp
@@ -0,0 +1,40 @@
+
+class Solution
+{
+public:
+        int dp[1101];
+    int solve(int index ,string &str , unordered_set<string>&s)
+    {
+        if(index >= str.length())
+        return 1;
+        
+        if(dp[index]!= -1)
+        return dp[index];
+        int ans = 0;
+        for(int i = index ; i<str.length() ; i++)
+        {
+            
+            if(s.count(str.substr(index , i- index +1)) != 0)
+            {
+                ans = ans || solve(i+1,str,s) ;
+            }
+        }
+        dp[index] = ans;
+        return ans;
+    }
+
+    unordered_set<string>s;
+    int wordBreak(string A, vector<string> &B) {
+        //code here
+        int n = B.size();
+        for(int i = 0 ; i<n ; i++)
+        {
+            s.insert(B[i]);
+        }
+        memset(dp,-1,sizeof(dp));
+        return solve(0,A,s);
+        
+        
+        
+    }
+};
