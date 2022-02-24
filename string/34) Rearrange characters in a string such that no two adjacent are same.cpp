6b963260ac18b1aee21b1a3fbec1f9c1c8eb422a
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2022-02-24 21:35:33 +0530

    Create 34) Rearrange characters in a string such that no two adjacent are same.cpp

---
+++ string/34) Rearrange characters in a string such that no two adjacent are same.cpp
@@ -0,0 +1,52 @@
+/*
+Here's the explanation -
+
+Eg -
+
+string=aaaabcc
+
+max frequency(f) = 4
+
+no. of spaces=f-1 = 3
+a_a_a_a
+no. of character that can be put in those spaces = length of string(n) - max frequency(f)
+
+So, if f-1<=n-f
+
+return 1
+
+else
+
+return 0
+*/
+
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	//code
+	int t ;
+	cin >>t;
+	while(t--)
+	{
+	    string s ;
+	    cin>>s;
+	    int maxx = 0;
+	    map<char,int>m;
+	    for(auto x:s)
+	    {
+	        m[x]++;
+	        maxx = max(maxx,m[x]);
+	    }
+	    if(maxx <= s.length() - maxx + 1)
+	    {
+	        cout<<1;
+	    }
+	    else cout<<0;
+	    cout<<endl;
+	    
+	}
+	return 0;
+}
