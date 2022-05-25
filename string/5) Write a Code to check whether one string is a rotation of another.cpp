74e0720efddbdb7f3caec8af3f0bd5fcd3c60240
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2022-05-25 13:35:31 +0530

    Update 5) Write a Code to check whether one string is a rotation of another.cpp

--- string/5) Write a Code to check whether one string is a rotation of another.cpp
+++ string/5) Write a Code to check whether one string is a rotation of another.cpp
@@ -1,3 +1,4 @@
+// method 1 
 // C++ program to check if two given strings
 // are rotations of each other
 # include <bits/stdc++.h>
@@ -26,3 +27,67 @@ else
 return 0;
 }
 
+
+// method 2  by queues
+Method 2(Using STL):
+
+Algorithm :
+
+1. If the size of both the strings is not equal, then it can never be possible.
+
+2. Push the original string into a queue q1.
+
+3. Push the string to be checked inside another queue q2.
+
+ 4. Keep popping q2‘s and pushing it back into it till the number of such operations are less than the size of the string.
+
+5. If q2 becomes equal to q1 at any point during these operations, it is possible. Else not.
+	
+	
+	code :
+
+#include <bits/stdc++.h>
+using namespace std;
+bool check_rotation(string s, string goal)
+{
+	if (s.size() != goal.size())
+		return false;
+	queue<char> q1;
+	for (int i = 0; i < s.size(); i++) {
+		q1.push(s[i]);
+	}
+	queue<char> q2;
+	for (int i = 0; i < goal.size(); i++) {
+		q2.push(goal[i]);
+	}
+	int k = goal.size();
+	while (k--) {
+		char ch = q2.front();
+		q2.pop();
+		q2.push(ch);
+		if (q2 == q1)
+			return true;
+	}
+	return false;
+}
+int main()
+{
+	string s1 = "ABCD";
+	string s2 = "CDAB";
+	if (check_rotation(s1, s2))
+		cout << s2 << " is a rotated form of " << s1
+			<< endl;
+	else
+		cout << s2 << " is not a rotated form of " << s1
+			<< endl;
+	string s3 = "ACBD";
+	if (check_rotation(s1, s3))
+		cout << s3 << " is a rotated form of " << s1
+			<< endl;
+	else
+		cout << s3 << " is not a rotated form of " << s1
+			<< endl;
+	return 0;
+}
+
+link https://www.geeksforgeeks.org/a-program-to-check-if-strings-are-rotations-of-each-other/#
