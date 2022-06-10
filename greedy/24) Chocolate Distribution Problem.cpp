9a651b10191540e50e2c5bbf21d042652e1dfbbb
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2022-06-11 00:27:03 +0530

    Create 24) Chocolate Distribution Problem.cpp

---
+++ greedy/24) Chocolate Distribution Problem.cpp
@@ -0,0 +1,18 @@
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> arr, long long n, long long m){
+    //code
+    sort(arr.begin(),arr.end());
+    
+    long long diff = arr[m-1] - arr[0];
+    
+    for(int i = m ; i<n  ; i++)
+    {
+      diff = min(diff,arr[i] - arr[i-m+1] );  
+    }
+    return diff;
+    
+     
+    
+    }   
+};
