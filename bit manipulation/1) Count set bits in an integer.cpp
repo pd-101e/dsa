8676236c0f253ad07a9a3e9d763d3082db46e7e9
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2022-07-20 22:20:00 +0530

    Create 1) Count set bits in an integer.cpp

---
+++ bit manipulation/1) Count set bits in an integer.cpp
@@ -0,0 +1,13 @@
+Class Solution{ 
+public:
+    int setBits(int N) {
+        // Write Your Code here
+        int count = 0 ;
+        while(N>0)
+        {
+            count++;
+            N = N & (N-1);
+        }
+        return count;
+        
+    }
